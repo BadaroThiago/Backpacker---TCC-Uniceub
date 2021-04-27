37ed1f2f1f84151eb6aacd7c5496671e45de14b2
Rafael Dias <rafa197795@gmail.com>
2021-04-26 22:48:57 -0300

    feat: cria base das telas de listar gastos e documentos

--- src/components/IconAndText.js
+++ src/components/IconAndText.js
@@ -1,17 +1,14 @@
-import React  from 'react';
-import {Text, View} from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 
-import { FontAwesome5 } from '@expo/vector-icons'; 
+import { FontAwesome5 } from "@expo/vector-icons";
 
-export const IconAndText = ({nome, text}) => {
+export const IconAndText = ({ nome, text }) => {
   return (
- 
-    <View style={{flexDirection:'row'}}>
-	    <FontAwesome5 name= {nome} size={20} color="#ECE9E6"></FontAwesome5>
-      
-      <Text style={{color:'#ECE9E6', marginLeft: 5}}>{text}</Text> 
-    </View>
-   
-  );
-}
+    <View style={{ flexDirection: "row" }}>
+      <FontAwesome5 name={nome} size={20} color="#ECE9E6"></FontAwesome5>
 
+      <Text style={{ color: "#ECE9E6", marginLeft: 5 }}>{text}</Text>
+    </View>
+  );
+};
